64cd88caa9f5e2e10d6f6a3dc11132efb30e85e4
tiohs <hamiltonsilva551@gmail.com>
2022-05-20 12:32:02 +0100

    Ok

--- Models Back/src/server.js
+++ Models Back/src/server.js
@@ -9,6 +9,20 @@ app.set('views', join(__dirname, 'APP', 'views'));
 app.use(urlencoded({ extended: false }));
 app.use(router);
 
+app.use((err, request,  response, _) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 500,
+    message: 'Interno Error ',
+  });
+});
+
 app.listen(3333, () => {
   console.log('Port on http://localhost:3333/')
 });
