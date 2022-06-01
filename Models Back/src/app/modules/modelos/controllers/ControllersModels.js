1cf29e53fd082bef4f2542a71277cb3c84bfa7d1
tiohs <hamiltonsilva551@gmail.com>
2022-06-01 17:42:47 +0100

    models

--- Models Back/src/app/modules/modelos/controllers/ControllersModels.js
+++ Models Back/src/app/modules/modelos/controllers/ControllersModels.js
@@ -3,13 +3,22 @@ import CreateModels from '../Services/ServiceCrateModels';
 class ControllerModels {
   async store(request, response){
     const dados = request.body;
-    const file = request.files;
+    const [file1, file2, file3] = request.files;
 
-    const createModels = new CreateModels();
-    await createModels.execute({ ...dados, photo : `${file}`})
+    try {
+      const createModels = new CreateModels();
+      await createModels.execute({ 
+        ...dados, 
+        photo : `${[file1.filename, file2.filename, file3.filename]
+        }`})
+      
+      request.flash('error', { type : 'success' , message : 'Cadastrado com sucesso !'});
+      return response.redirect('/admin/models/add');
+    } catch(err){
+      request.flash('error', { type : 'error' , message : 'Usário não cadastrado !'});
+      return response.redirect('/admin/models/add');
+    }
     
-    
-    response.redirect('/admin/models/add');
     
   }
 }
